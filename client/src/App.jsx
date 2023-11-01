7e9220963ca64d7d5f4c3f58c6323063eb6c7c8e
Jamshadj <jamshadalij2004@gmail.com>
2023-11-01 10:14:03 +0530

    removed un necessary logs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
 import SignupPage from './pages/SignUpPage/SignupPage';
-import LoginComponent from './componet/LoginComponent/LoginComponent';
+import LoginComponent from './component/LoginComponent/LoginComponent';
 
 function App() {
+  const token = localStorage.getItem('token');
+
   return (
     <Router>
       <div className="App">
         <Routes>
-          <>
-          <Route path="/user/tasks" element={<HomePage/>} />
-            <Route path="/user/signup" element={<SignupPage />} />
-            <Route path="/user/login" element={<LoginComponent />} />
-          </>
+          <Route
+            path="/"
+            element={
+              token ? (
+                <Navigate to="/user/tasks" />
+              ) : (
+                <Navigate to="/user/login" />
+              )
+            }
+          />
+          <Route path="/user/tasks" element={<HomePage />} />
+          <Route path="/user/signup" element={<SignupPage />} />
+          <Route path="/user/login" element={<LoginComponent />} />
         </Routes>
       </div>
     </Router>
